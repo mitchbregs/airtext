cab37746d402b92b86d26e1342cfc020597d5e14
Mitchell Bregman <mitchbregs@Mitchells-Air.hsd1.nj.comcast.net>
2022-08-14 22:46:57 -0400

    Update version

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 python3 -m venv .venv
 source .venv/bin/activate
+pip install awscli
+aws codeartifact login --tool twine --repository airtext-pypi-store --domain airtext-pypi --domain-owner 312590578399
 pip install -e .
 pip install -e '.[dev,pkg]'
 python -m build --sdist --wheel --outdir dist/ .
-aws codeartifact login --tool twine --repository airtext-pypi-store --domain airtext-pypi --domain-owner 312590578399
 twine upload --verbose -r codeartifact dist/*
 # export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain airtext-pypi --domain-owner 312590578399 --query authorizationToken --output text`
 # pip config set global.index-url https://aws:$CODEARTIFACT_AUTH_TOKEN@airtext-pypi-312590578399.d.codeartifact.us-east-1.amazonaws.com/pypi/airtext-pypi-store/simple/
