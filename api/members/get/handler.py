4676ac7c8c6c340029f16316536d3664cb456ce0
Ubuntu <ubuntu@ip-172-31-80-118.ec2.internal>
2022-09-02 04:26:24 +0000

    Member get

--- api/members/get/handler.py
+++ api/members/get/handler.py
@@ -16,20 +16,50 @@ def main(event: Dict, context: Dict) -> None:
     params = event["queryStringParameters"]
 
     airtext = AirtextAPI()
+    id_ = params.get("id")
     proxy_number = params.get("proxy_number")
 
-    try:
-        result = airtext.members.get_by_proxy_number(proxy_number=proxy_number)
-        member = result.to_dict()
-    except Exception as e:
-        logger.info(e)
-        raise e
+    if id_:
+        try:
+            member = airtext.members.get_by_id(id=id_)
+        except Exception as e:
+            logger.error(e)
+            return {
+                "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "body": json.dumps("Member not found using `id` provided."),
+                "isBase64Encoded": False,
+            }
+    elif proxy_number:
+        try:
+            member = airtext.members.get_by_proxy_number(proxy_number=proxy_number)
+        except Exception as e:
+            logger.error(e)
+            return {
+                "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "body": json.dumps("Member not found using `proxy_number` provided."),
+                "isBase64Encoded": False,
+            }
+    else:
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps("Must provide `proxy_number` or `id` parameter."),
+            "isBase64Encoded": False,
+        }
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
         },
-        "body": json.dumps(member, default=str),
+        "body": member.to_json(),
         "isBase64Encoded": False,
     }
