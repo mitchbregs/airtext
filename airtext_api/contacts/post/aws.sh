336bf160bb7d2b054fd8416d99542e9406da17d2
Ubuntu <ubuntu@ip-172-31-80-118.ec2.internal>
2022-09-05 21:19:44 +0000

    Contacts post

--- airtext_api/contacts/post/aws.sh
+++ airtext_api/contacts/post/aws.sh
@@ -1,17 +1,31 @@
-echo "Creating role..."
-aws iam create-role --role-name codebuild-airtext-api-dev-contacts-post-service-role --assume-role-policy-document '{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": "codebuild.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
+export ENVIRONMENT=prod
+echo "Using '${ENVIRONMENT}' environment..."
+echo "Creating ECR service role..."
+aws iam create-role --role-name codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role --assume-role-policy-document '{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": "codebuild.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
 echo "Attaching role policies..."
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess --role-name codebuild-airtext-api-dev-contacts-post-service-role
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess --role-name codebuild-airtext-api-dev-contacts-post-service-role
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSCodeArtifactReadOnlyAccess --role-name codebuild-airtext-api-dev-contacts-post-service-role
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --role-name codebuild-airtext-api-dev-contacts-post-service-role
-echo "Creating CodeBuild project..."
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess --role-name codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess --role-name codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSCodeArtifactReadOnlyAccess --role-name codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --role-name codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role
+echo "Creating Lambda service role..."
+aws iam create-role --role-name lambda-${ENVIRONMENT}-airtext-api-contacts-post-cloudwatch-service-role --assume-role-policy-document '{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
+echo "Attaching role policies..."
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --role-name lambda-${ENVIRONMENT}-airtext-api-contacts-post-cloudwatch-service-role
+echo "Creating ECR repository"
+aws ecr create-repository --repository-name ${ENVIRONMENT}-airtext-api-contacts-post-lambda
+echo "Configuring roles..."
 sleep 10s
-aws codebuild \
-create-project \
---name dev-airtext-api-contacts-post \
---source '{"type": "GITHUB", "location": "https://github.com/mitchbregs/airtext", "buildspec": "api/contacts/post/buildspec.yml"}' \
+echo "Creating Lambda function"
+aws lambda create-function \
+--function-name ${ENVIRONMENT}-airtext-api-contacts-post \
+--role arn:aws:iam::312590578399:role/lambda-${ENVIRONMENT}-airtext-api-contacts-post-cloudwatch-service-role \
+--code ImageUri=312590578399.dkr.ecr.us-east-1.amazonaws.com/base:latest \
+--package-type Image
+echo "Creating CodeBuild project..."
+aws codebuild create-project \
+--name ${ENVIRONMENT}-airtext-api-contacts-post-ecr \
+--source '{"type": "GITHUB", "location": "https://github.com/mitchbregs/airtext", "buildspec": "airtext_api/contacts/post/buildspec.'${ENVIRONMENT}'.yml"}' \
 --artifacts '{"type": "NO_ARTIFACTS"}' \
 --environment '{"type": "LINUX_CONTAINER", "image": "aws/codebuild/standard:6.0", "privilegedMode": true, "computeType": "BUILD_GENERAL1_SMALL"}' \
---service-role codebuild-airtext-api-dev-contacts-post-service-role \
+--service-role codebuild-${ENVIRONMENT}-airtext-api-contacts-post-ecr-service-role \
 --logs-config '{"cloudWatchLogs": {"status": "ENABLED"}}'
