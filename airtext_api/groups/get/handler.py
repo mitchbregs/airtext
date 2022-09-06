37b4742cc0e7c29b6d4a61fd87f186ad50e0ab8e
Ubuntu <ubuntu@ip-172-31-80-118.ec2.internal>
2022-09-06 23:38:35 +0000

    Groups get

--- airtext_api/groups/get/handler.py
+++ airtext_api/groups/get/handler.py
@@ -16,15 +16,22 @@ def main(event: Dict, context: Dict) -> None:
     params = event["queryStringParameters"]
 
     airtext = AirtextAPI()
-    proxy_number = params.get["proxy_number"]
-    # proxy_number = params.get("member_id")
+    proxy_number = params["proxy_number"]
+    # member_id = params.get("member_id")
 
     try:
         results = airtext.groups.get_by_proxy_number(proxy_number=proxy_number)
         groups = [row.to_dict() for row in results]
     except Exception as e:
         logger.info(e)
-        raise e
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps("Unable to retrieve groups."),
+            "isBase64Encoded": False,
+        }
 
     return {
         "statusCode": 200,
